7050b44f41fc3f844e5fb1b7f6ffc97027d1e264
= <=>
2023-11-23 22:51:01 -0800

    Added sequelize logic for creating/updating/deleting recruiters and students

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,26 +1,132 @@
 const Account = require('../models/accountModel');
 // HANDLES BOTH STUDENTS AND RECRUITERS
 
-exports.createStudent = (req, res) => {
+exports.createStudent = async (req, res) => {
     // Logic to create a new student
+    try {
+        const {FirstName, LastName, School, EmailID, AcademicYear, Age, AcademicMajor, GPA, WorkExperience, PersonalStatement, Experience} = req.body;
+
+        const newStudent = await Account.Student.create({
+            FirstName,
+            LastName,
+            School,
+            EmailID,
+            AcademicYear,
+            Age,
+            AcademicMajor,
+            GPA,
+            WorkExperience,
+            PersonalStatement,
+            Experience
+        });
+
+        res.status(201).send(newStudent);
+
+    } catch (error) {
+        res.status(400).send({message: "Error creating new student", error: error.message});
+    }
 };
 
-exports.updateStudent = (req, res) => {
+exports.updateStudent = async (req, res) => {
     // Logic to update a student by ID
+    try {
+        const studentID = req.params.studentID //Need to be able to get studentID
+        const updatedData = req.body;
+
+        const [updated] = await Account.Student.update(updatedData, {
+            where: {id: studentID}
+        });
+
+        if (updated) {
+            const updatedStudent = await Account.Student.findByPk(studentID);
+            res.status(200).send(updatedStudent);
+        } else {
+            res.status(404).send({message: "Student not found"});
+        }
+    } catch (error) {
+        res.status(400).send({message: "Error updating student", error: error.message});
+
+    }
 };
 
-exports.deleteStudent = (req, res) => {
+exports.deleteStudent = async (req, res) => {
     // Logic to delete a student by ID
+    try {
+        const deleteID = req.params.studentID //Need to be able to get studentID
+
+        const toDelete = await Account.Student.destroy({
+            where: {id: deleteID}
+        });
+
+        if (toDelete) {
+            res.status(200).send({message: "Successfully deleted student"});
+        } else {
+            res.status(404).send({message: "Student not found"});
+        }
+    } catch (error) {
+        res.status(400).send({message: "Error deleting student", error: error.message});
+    }
 };
 
-exports.createRecruiter = (req, res) => {
-    // Logic to create a new recruiter
+exports.createRecruiter = async (req, res) => {
+    try {
+        const {RecruiterID, FirstName, LastName, CompanyID, EmailID, CompanyName, Roles, Locations} = req.body;
+
+        const newRecruiter = await Account.Recruiter.create({
+            RecruiterID,
+            FirstName,
+            LastName,
+            CompanyID,
+            EmailID,
+            CompanyName,
+            Roles,
+            Locations
+        });
+
+        res.status(201).send(newRecruiter);
+
+    } catch (error) {
+        res.status(400).send({message: "Error creating new recruiter", error: error.message});
+    }
 };
 
-exports.updateRecruiter = (req, res) => {
+exports.updateRecruiter = async (req, res) => {
     // Logic to update a recruiter by ID
+    try {
+        const recruiterID = req.params.recruiterID //Need to be able to get recruiterID
+        const updatedData = req.body;
+
+        const [updated] = await Account.Recruiter.update(updatedData, {
+            where: {id: recruiterID}
+        });
+
+        if (updated) {
+            const updatedRecruiter = await Account.Recruiter.findByPk(recruiterID);
+            res.status(200).send(updatedRecruiter);
+        } else {
+            res.status(404).send({message: "Recruiter not found"});
+        }
+    } catch (error) {
+        res.status(400).send({message: "Error updating recruiter", error: error.message});
+
+    }
 };
 
-exports.deleteRecruiter = (req, res) => {
+exports.deleteRecruiter = async (req, res) => {
     // Logic to delete a recruiter by ID
+    try {
+        const deleteID = req.params.recruiterID //Need to be able to get recruiterID
+
+        const toDelete = await Account.Recruiter.destroy({
+            where: {id: deleteID}
+        });
+
+        if (toDelete) {
+            res.status(200).send({message: "Successfully deleted recruiter"});
+        } else {
+            res.status(404).send({message: "Recruiter not found"});
+        }
+    } catch (error) {
+        res.status(400).send({message: "Error deleting recruiter", error: error.message});
+    }
 };
