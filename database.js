43e541fd73ed86093580c0e7a7976f9689dc221a
sh2002vk <sh2002vk@gmail.com>
2024-08-03 16:07:43 -0700

    live db connection

--- database.js
+++ database.js
@@ -3,9 +3,16 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('wiptest', process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'postgres',  // Change dialect to 'postgres'
+  dialect: 'postgres',
+  port: process.env.DB_PORT || 5432,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Set to false if using self-signed certificates
+    },
+  },
   logging: false,
 });
 
@@ -14,7 +21,7 @@ sequelize.authenticate()
     console.log('Connection has been established successfully.');
   })
   .catch(err => {
-    console.log('Unable to connect to the database:', err);
+    console.log('Unable to connect to the database (seq):', err);
   });
 
 module.exports = sequelize;
--- db_creation.js
+++ db_creation.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: 'wiptest',
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 5432,
+    ssl: {
+      rejectUnauthorized: false, // Set to false if using self-signed certificates
+    },
+  });
 
 client.connect(err => {
   if (err) {
